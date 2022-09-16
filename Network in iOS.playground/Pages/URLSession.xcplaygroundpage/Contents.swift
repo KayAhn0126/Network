9fb4be20084f9d34a0be18f05f8b3894c3efef80
KayAhn0126 <dkseotn123@gmail.com>
2022-09-16 19:46:30 +0900

    feat: URLSession 기본 정리

--- Network in iOS.playground/Pages/URLSession.xcplaygroundpage/Contents.swift
+++ Network in iOS.playground/Pages/URLSession.xcplaygroundpage/Contents.swift
@@ -4,6 +4,22 @@ import Foundation
 
 // configuration -> urlsession -> urlsessionTask
 
+let configuration = URLSessionConfiguration.default
+let session = URLSession(configuration: configuration)
+let url = URL(string: "https://api.github.com/users/kayahn0126")!
+
+let task = session.dataTask(with: url) { data, response, error in
+    guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+        print("--> response \(response)")
+        return
+    }
+    
+    guard let data = data else { return }
+    
+    let result = String(data: data, encoding: .utf8)
+    print(result)
+}
+task.resume()
 
 
 
