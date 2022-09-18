60ce04b318d271fac0261cbd5512b74e2bba8564
KayAhn0126 <dkseotn123@gmail.com>
2022-09-18 13:51:06 +0900

    refactor: 네트워크 클래스 구성 및 정리

--- Network in iOS.playground/Pages/Fetch Method.xcplaygroundpage/Contents.swift
+++ Network in iOS.playground/Pages/Fetch Method.xcplaygroundpage/Contents.swift
@@ -26,6 +26,57 @@ struct GithubProfile: Codable {
     }
 }
 
+final class NetworkService {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
+    
+    func fetchProfile(userName: String, completion: @escaping (Result<GithubProfile, Error>) -> Void) {
+        let url = URL(string: "https://api.github.com/users/\(userName)")!
+
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                completion(.failure(NetworkError.transportError(error)))
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse, !(200..<300).contains(httpResponse.statusCode) {
+                completion(.failure(NetworkError.responseError(statusCode: httpResponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            // data -> GithubProfile
+            
+            do {
+                let decoder = JSONDecoder()
+                let profile = try decoder.decode(GithubProfile.self, from: data)
+                completion(.success(profile))
+            } catch let error as NSError {
+                completion(.failure(NetworkError.decodingError(error)))
+            }
+        }
+        task.resume()
+    }
+}
+
+// network 담당 NetworkService
+// NetworkService 이용한 네트워크 작업
+
+let network = NetworkService(configuration: .default)
+network.fetchProfile(userName: "kayahn0126") { result in
+    switch result {
+    case .success(let profile):
+        print("Received: \(profile)")
+    case .failure(let error):
+        print("Error: \(error)")
+    }
+}
 
 
 
